07799c5682cc2111f05f8b682205750b2bb91a6b
risa.iwata <r_iwata@allexceed.co.jp>
2020-05-11 18:41:50 +0900

    add getAllMessages

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "messages")
 public class Message {
     @Id
@@ -29,43 +37,43 @@ public class Message {
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
-    public Integer getId(){
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Integer id){
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title){
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getContent(){
+    public String getContent() {
         return content;
     }
 
-    public void setContent(String content){
+    public void setContent(String content) {
         this.content = content;
     }
 
-    public Timestamp getCreated_at(){
+    public Timestamp getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(Timestamp created_at){
+    public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
     }
-    public Timestamp getUpdated_at(){
+
+    public Timestamp getUpdated_at() {
         return updated_at;
     }
 
-    public void setUpdated_at(Timestamp updated_at){
+    public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
-
 }
